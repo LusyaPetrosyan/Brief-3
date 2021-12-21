5fd2dfb63c8d67459b22d9e7c9764ab6e3dd5b2b
LusyaPetrosyan <plusine99@gmail.com>
2021-12-21 22:30:48 +0400

    Brief 3

---
+++ src/com/company/commons/constants/FileConstants.java
@@ -0,0 +1,8 @@
+package com.company.commons.constants;
+
+public class FileConstants {
+    public static final String IMAGE_RESOURCE_PATH = "./Img/";
+    public static final String WHERE_PATH = "/Desktop/Խելք";
+    public static final String FOLDER_NAME = "Brief1";
+    public static final String USER_CV_DATA = "data.js";
+}
